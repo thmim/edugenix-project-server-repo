8ac35059562b952af34150148bde115ae584fa95
thmim <mdtaharimmim1010@gmail.com>
2025-07-22 14:01:12 +0600

    post assignment data and teacher review data

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ async function run() {
     const usersCollection = client.db('teacherDB').collection('users')
     const paymentsCollection = client.db('teacherDB').collection('payments')
     const assignmentsCollection = client.db('teacherDB').collection('assignments')
+    const assignmentSubmissionsCollection = client.db('teacherDB').collection('submissions')
+    const reviewsCollection = client.db('teacherDB').collection('reviews')
 
 
     // Search user by email or name (partial match)
@@ -186,6 +188,101 @@ async function run() {
       }
     });
 
+    // Count enrollments
+    // app.get('/enrollments/count/:id', async (req, res) => {
+
+    //   const count = await addClassCollection.countDocuments({ classId: req.params.id });
+    //   res.send({ count });
+    // });
+
+    // for (const payment of enrolledClasses) {
+    //       const courseDetails = await addClassCollection.findOne({ _id: new ObjectId(payment.courseId) });
+    //       if (courseDetails) {
+    //         enrolledClassesDetails.push({
+    //           ...payment,
+    //           courseTitle: courseDetails.title,
+    //           instructorName: courseDetails.name,
+    //           courseImage: courseDetails.image,
+    //         });
+    //       }
+    //     }
+    //     app.get('/classes/assignment/:classId', async (req, res) => {
+    //   try {
+    //     const classId = req.params.classId;
+    //     const classObjectId = new ObjectId(classId); 
+
+    //     const classDetails = await addClassCollection
+    //     .findOne({ _id: classObjectId })
+    //     .toArray()
+    //     ;
+    //     const classesProgressDetails = [];
+    //     for(const progressData of classDetails){
+    //       const submissionCount = await assignmentSubmissionsCollection.findOne({ classObjectId: new ObjectId(classId) });
+    //     if (submissionCount) {
+    //             classesProgressDetails.push({
+    //               ...progressData,
+    //               submission_count:submissionCount.submission_count
+    //             });
+
+    //     }
+
+    //     }
+
+
+    //     // if (!classDetails) {
+    //     //   return res.status(404).send({ message: 'Class not found' });
+    //     // }
+
+
+    //     // const totalSubmissionCount = await assignmentSubmissionsCollection.countDocuments({ courseId: classId });
+
+
+    //     // const responseData = {
+    //     //   ...classDetails,
+    //     //   submission_count: totalSubmissionCount,
+    //     // };
+
+    //     res.send(responseData);
+    //   } catch (error) {
+    //     console.error('Error fetching class details:', error);
+    //     res.status(500).send({ error: 'Failed to fetch class details' });
+    //   }
+    // });
+
+    app.get('/classes/assignment/:classId', async (req, res) => {
+      try {
+        const classId = req.params.classId;
+        const classObjectId = new ObjectId(classId);
+        const classDetails = await addClassCollection.findOne({ _id: classObjectId });
+        if (!classDetails) {
+          return res.status(404).send({ message: 'Class not found' });
+        }
+
+        const enrollmentCount = classDetails.enrollmentCount || 0;
+        const assignmentCount = classDetails.assignment_count || 0;
+
+        const assignmentsInClass = await assignmentsCollection.find({ classId: new ObjectId(classId) }).toArray();
+        console.log(assignmentsInClass);
+        let totalSubmissionCount = 0;
+        assignmentsInClass.forEach(assignment => {
+          totalSubmissionCount += assignment.submission_count || 0;
+        });
+        console.log(totalSubmissionCount);
+        const responseData = {
+
+          enrollmentCount: enrollmentCount,
+          assignmentCount: assignmentCount,
+          totalSubmissionCount: totalSubmissionCount,
+        };
+
+        res.send(responseData);
+      } catch (error) {
+        console.error('Error fetching class details:', error);
+        res.status(500).send({ error: 'Failed to fetch class details' });
+      }
+    });
+
+
     // GET all assignments by class ID
     app.get('/assignments/:courseId', async (req, res) => {
       const classId = req.params.courseId
@@ -195,26 +292,48 @@ async function run() {
 
     // post assignment data
     app.post('/assignments', async (req, res) => {
-      const { title, description, deadline, create_date, assignment_count, submission_count, id } = req.body;
+      const { title, description, deadline, created_by, createdAt, submission_count, id } = req.body;
       const assignment = {
         title: title,
         description: description,
         deadline: deadline,
-        create_date: create_date,
-        assignment_count: assignment_count,
+        createdAt: createdAt,
         submission_count: submission_count,
         classId: new ObjectId(id),
+        created_by: created_by
 
       }
       const result = await assignmentsCollection.insertOne(assignment);
       // increase assignment count
       if (result.insertedId) {
-      
-      await addClassCollection.updateOne( 
-        { _id: new ObjectId(assignment.classId) },
-        { $inc: { assignmentCount: 1 } } 
-      );
-    }
+
+        await addClassCollection.updateOne(
+          { _id: new ObjectId(assignment.classId) },
+          { $inc: { assignment_count: 1 } }
+        );
+      }
+      res.send(result);
+    });
+
+    // POST a submission and update submission count
+    app.post('/submissions', async (req, res) => {
+
+      const submitData = req.body;
+      const { submitBy, assignmentId } = req.body;
+      const alreadySubmitted = await assignmentSubmissionsCollection.findOne({ submitBy, assignmentId });
+      if (alreadySubmitted) {
+        return res.status(409).send({ message: "Assignment already submitted" });
+      }
+
+      const result = await assignmentSubmissionsCollection.insertOne(submitData);
+
+      if (result.insertedId) {
+        await assignmentsCollection.updateOne(
+          { _id: new ObjectId(submitData.assignmentId) },
+          { $inc: { submission_count: 1 } }
+        );
+      }
+
       res.send(result);
     });
 
@@ -335,6 +454,30 @@ async function run() {
         });
       } catch (error) {
         res.status(400).send({ error: error.message });
+      }
+    });
+
+    // POST TER (Teacher Evaluation & Review)
+    app.post('/teacherEvaluation', async (req, res) => {
+      try {
+        const {rating,description,courseId,createdAt,studentName,studentEmail} = req.body;
+
+        const result = await reviewsCollection.insertOne({
+          rating:rating,
+          description:description,
+          studentName:studentName,
+          studentEmail:studentEmail,
+          createdAt:createdAt,
+          courseId:new ObjectId(courseId)
+        });
+
+        res.status(201).send({
+          message: 'TER submitted successfully.',
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error('Error posting TER:', error);
+        res.status(500).send({ message: 'Failed to submit TER.' });
       }
     });
 
