2736f9b067448a4b511de98b90041e550d2c9a01
thmim <mdtaharimmim1010@gmail.com>
2025-08-17 23:31:55 +0600

    remove unecessary code

--- index.js
+++ index.js
@@ -681,7 +681,7 @@ async function run() {
     });
 
 
-    // Send a ping to confirm a successful connection
+    
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
